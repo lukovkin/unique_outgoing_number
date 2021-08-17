a0683e6898086520982d924f89ac1f02d135fc15
Dmitry Lukovkin <lukovkin@users.noreply.github.com>
2021-08-17 20:30:00 +0300

    Update unique_outgoing_num_bot.py

--- unique_outgoing_num_bot.py
+++ unique_outgoing_num_bot.py
@@ -29,8 +29,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    yield "Hello world!"
     updater.start_polling()
+    return "Hello world!"
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
