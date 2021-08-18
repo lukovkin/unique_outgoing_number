e1274afff84e10774764d5246aa3e145c5665d3e
Dmitry Lukovkin <lukovkin@users.noreply.github.com>
2021-08-18 20:03:49 +0300

    Update unique_outgoing_num_bot.py

--- unique_outgoing_num_bot.py
+++ unique_outgoing_num_bot.py
@@ -9,6 +9,7 @@ import logging
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                      level=logging.INFO)
+logger = logging.getLogger()
 
 updater = Updater(token='1942145754:AAHp8-mw7lOIBKCwvSDywe3usrlmGzmok0k', use_context=True)
 dispatcher = updater.dispatcher
@@ -18,6 +19,7 @@ def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
     
 def test(update, context):
+    logger.info(str(context))
     context.bot.send_message(chat_id=update.effective_chat.id, text="Test passed. User data: {}".format(str(context.user_data)))
     
 def outgoing_id(update, context):
